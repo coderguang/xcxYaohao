c620cece67b1e5c69d6483fe47331301f2cdf012
youai <royalchen@royalchen.com>
2019-09-25 09:58:03 +0800

    history update db

--- src/db/handler.go
+++ src/db/handler.go
@@ -8,7 +8,7 @@ import (
 
 func UpdateDownloadToDb(data *define.DownloadHistoryUrl) {
 	//err := globalDb.Where(define.DownloadHistoryUrl{Title: data.Title, URL: data.URL}).Assign(define.DownloadHistoryUrl{Status: data.Status, Tips: data.Tips}).FirstOrCreate(data).Error
-	err := globalDb.Where(define.DownloadHistoryUrl{Title: data.Title, URL: data.URL}).Assign(define.DownloadHistoryUrl{}).FirstOrCreate(data).Error
+	err := globalDb.Where(define.DownloadHistoryUrl{Title: data.Title, URL: data.URL}).Assign(define.DownloadHistoryUrl{Status: data.Status, Tips: data.Tips}).FirstOrCreate(data).Error
 	if err != nil {
 		sglog.Error("update download url db error,", err)
 	}

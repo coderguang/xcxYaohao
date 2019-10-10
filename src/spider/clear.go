9244926f9d20de0ee56b06a9719d810a079ac642
youai <royalchen@royalchen.com>
2019-10-10 12:21:03 +0800

    fix clear data

--- src/spider/clear.go
+++ src/spider/clear.go
@@ -17,7 +17,7 @@ func InitClear() {
 		nowTime := time.Now()
 		normalTime := time.Date(nowTime.Year(), nowTime.Month(), nowTime.Day(), 23, 59, 59, 0, nowTime.Location())
 		timeInt := normalTime.Sub(nowTime)
-		sleepTime := int(timeInt/time.Second) + 1 // +1 for avoid loop run in that second time
+		sleepTime := int(timeInt/time.Second) + 10 // +1 for avoid loop run in that second time
 		sglog.Info("next clear timer will run after ", sleepTime, " seconds in ", sleepTime, sgtime.NormalString(sgtime.TransfromTimeToDateTime(normalTime)))
 		//sleepTime = 60
 		sgthread.SleepBySecond(sleepTime)

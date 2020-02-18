94a250a5dd65a6ce46b8d6f99072467f36a11e65
pchome <royalchen@royalchen.com>
2020-02-19 01:17:16 +0800

    ali broadcast

--- src/httpHandle/time.go
+++ src/httpHandle/time.go
@@ -14,12 +14,15 @@ func requireLastestTime(r *http.Request, city string, openId string, platform st
 	returnData[HTTP_RETURN_ERR_CODE] = YAOHAO_OK
 	returnData[HTTP_RETURN_TIME] = timestr
 	returnData[HTTP_RETURN_TIPS] = data.GetBoardcast(city)
+	if platform == "alipay" {
+		returnData[HTTP_RETURN_TIPS] = ""
+	}
 	returnData[HTTP_RETURN_Data] = openId
 
 	scenId := r.FormValue(HTTP_ARGS_SCENE_ID)
 	shareBy := r.FormValue(HTTP_ARGS_SHARE_FROM)
 
-	existData, shareByData := data.AddOpenXcxTimes(platform,openId, city, scenId, shareBy)
+	existData, shareByData := data.AddOpenXcxTimes(platform, openId, city, scenId, shareBy)
 	db.UpdateNoticeData(existData)
 	//sglog.Info("scenEId:", scenId, ",shareBy:", shareBy)
 	if shareByData != nil && shareByData.Token != "" {
